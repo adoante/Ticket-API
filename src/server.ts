8e4d11a5d431e285902230617151c303f83b6078
Adolfo Gante <adolfogante@gmail.com>
2025-10-10 16:16:21 -0700

    switched to json

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,6 @@ const prisma = new PrismaClient()
 const app: Express = express();
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 3000
 
